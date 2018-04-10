56803a94d01392cb385b41ab6c82468d3fe13d87
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-04-10 15:06:34 -0700

    Add Simple Codec that doesn't do any gzinflate or deftating
    This helps sites that do not have that library installed.

--- sync/class.jetpack-sync-sender.php
+++ sync/class.jetpack-sync-sender.php
@@ -3,6 +3,7 @@
 require_once dirname( __FILE__ ) . '/class.jetpack-sync-queue.php';
 require_once dirname( __FILE__ ) . '/class.jetpack-sync-defaults.php';
 require_once dirname( __FILE__ ) . '/class.jetpack-sync-json-deflate-array-codec.php';
+require_once dirname( __FILE__ ) . '/class.jetpack-sync-simple-codec.php';
 require_once dirname( __FILE__ ) . '/class.jetpack-sync-modules.php';
 require_once dirname( __FILE__ ) . '/class.jetpack-sync-settings.php';
 
@@ -286,6 +287,13 @@ class Jetpack_Sync_Sender {
 	function get_codec() {
 		return $this->codec;
 	}
+	function set_codex() {
+		if ( function_exists( 'gzinflate' ) ) {
+			$this->codec           = new Jetpack_Sync_JSON_Deflate_Array_Codec();
+		} else {
+			$this->codec           = new Jetpack_Sync_Simple_Codec();
+		}
+	}
 
 	function send_checksum() {
 		require_once 'class.jetpack-sync-wp-replicastore.php';
@@ -346,10 +354,12 @@ class Jetpack_Sync_Sender {
 		$this->max_dequeue_time = $seconds;
 	}
 
+
+
 	function set_defaults() {
 		$this->sync_queue      = new Jetpack_Sync_Queue( 'sync' );
 		$this->full_sync_queue = new Jetpack_Sync_Queue( 'full_sync' );
-		$this->codec           = new Jetpack_Sync_JSON_Deflate_Array_Codec();
+		$this->set_codex();
 
 		// saved settings
 		Jetpack_Sync_Settings::set_importing( null );
